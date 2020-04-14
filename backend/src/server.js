a6335ad73184ef8ae28a5192d1535452008b7b25
Francisco <ciscoproo0@gmail.com>
2020-04-14 12:31:55 -0300

    first commit, backend and web uploaded

---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(process.env.PORT);
+
+console.log(`Server listen on port ${process.env.PORT}`);
---
+++ gobarber-web/src/pages/SignUp/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import * as Yup from 'yup';
+
+import logo from '../../assets/images/logo.svg';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'Senha de no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Criar Conta'}
+        </button>
+
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
